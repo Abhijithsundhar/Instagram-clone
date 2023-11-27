bb00752e49f3fc7dc0a9e77c275e9f838ee1844f
Abhijith K <121933064+Abhijithsundhar@users.noreply.github.com>
2023-11-27 10:02:38 +0530

    Update Authentication.dart

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -7,7 +7,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firstui/home.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_constants.dart';
 import 'homePage.dart';
 import 'models/postModel.dart';
